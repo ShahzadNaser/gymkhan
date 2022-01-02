e34f14bbe45a422dbcf9bd401ce24b6af7c7549b
Shahzad Naser <shahzadnaser1122@gmail.com>
2022-01-02 21:15:33 +0500

    update js to scan and update infromation accordingly

--- gymkhana/public/js/payment_entry.js
+++ gymkhana/public/js/payment_entry.js
@@ -1,23 +1,23 @@
 frappe.ui.form.on('Payment Entry', {
 	refresh(frm) {
-        console.log("=============================");
 		if(frm.doc.party){
             setTimeout(function(){
                 if(frm.doc.member_image)
-                    $('[data-fieldname="html_view"]').html('<div class="image text-center" style="height: 100px !important;width: 120px !important;"><img src="'+frm.doc.member_image+'"></div>');
-            }, 100);
-                
+                    $('[data-fieldname="html_view"]').html('<div class="image text-center" style="height: 130px !important;width: 120px !important;"><img style="max-height: 130px;" src="'+frm.doc.member_image+'"></div>');
+            }, 100);   
         }
 	},
 	party:function(frm){
         $('[data-fieldname="html_view"]').html('');
-        console.log("=============================");
         if(frm.doc.party){
             setTimeout(function(){
                 if(frm.doc.member_image)
-                    $('[data-fieldname="html_view"]').html('<div class="image text-center" style="height: 100px !important;width: 120px !important;"><img src="'+frm.doc.member_image+'"></div>');
-            }, 1000);
+                    $('[data-fieldname="html_view"]').html('<div class="image text-center" style="height: 130px !important;width: 120px !important;"><img style="max-height: 130px;" src="'+frm.doc.member_image+'"></div>');
+            }, 100);
                 
         }
+    },
+    scan_barcode: function(frm) {
+		frm.set_value('party', frm.doc.scan_barcode);
     }
-})
+});
